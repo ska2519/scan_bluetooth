53155e69b63a437d05db1179e2d4000537da74f0
Ska Lee <ska2519@gmail.com>
2022-08-20 08:47:16 +0300

    Feat/app-exception-handling close #23 Feat/error-logger close #22 Feat/setup-freezed close #19

--- lib/src/runner.dart
+++ lib/src/runner.dart
@@ -4,12 +4,15 @@ import 'dart:io';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../flavors.dart';
 import 'app.dart';
+import 'exceptions/async_error_logger.dart';
+import 'exceptions/error_logger.dart';
 import 'features/admob/application/admob_service.dart';
 import 'firebase_options.dart';
 import 'firebase_options_dev.dart';
@@ -17,28 +20,37 @@ import 'localization/string_hardcoded.dart';
 
 class AppRunner {
   static Future<void> run(Flavor flavor) async {
-    FirebaseAnalytics analytics;
+    late ErrorLogger errorLogger;
+    FirebaseAnalytics? analytics;
     // * For more info on error handling, see:
     // * https://docs.flutter.dev/testing/errors
     await runZonedGuarded(() async {
       WidgetsFlutterBinding.ensureInitialized();
+      // turn off the # in the URLs on the web
+      GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+
       await Firebase.initializeApp(
         options: flavor == Flavor.PROD
             ? DefaultFirebaseOptions.currentPlatform
             : DefaultFirebaseOptionsDev.currentPlatform,
       );
-      analytics = FirebaseAnalytics.instance;
-      await analytics.logAppOpen();
-      await analytics.logEvent(name: 'app_start');
-      FlutterError.onError =
-          FirebaseCrashlytics.instance.recordFlutterFatalError;
+      if (kReleaseMode) {
+        analytics = FirebaseAnalytics.instance;
+        await analytics!.logAppOpen();
+        await analytics!.logEvent(name: 'app_start');
+        FlutterError.onError =
+            FirebaseCrashlytics.instance.recordFlutterFatalError;
+      }
+
       // FirebaseCrashlytics.instance.crash();
-      final container = ProviderContainer();
+      final container = ProviderContainer(
+        observers: [AsyncErrorLogger()],
+      );
+      errorLogger = container.read(errorLoggerProvider);
       if (Platform.isAndroid || Platform.isIOS) {
         container.read(admobServiceProvider);
       }
-      // turn off the # in the URLs on the web
-      GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+
       // * Entry point of the app
       runApp(UncontrolledProviderScope(
         container: container,
@@ -59,9 +71,10 @@ class AppRunner {
         );
       };
     }, (Object error, StackTrace stack) {
-      // * Log any errors to console
-      debugPrint('debugPrint error: ${error.toString()}');
-      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      if (kReleaseMode) {
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      }
+      errorLogger.logError(error, stack);
     });
   }
 }

4003f3b6edfd1d7e005177cb959c25bdad0071e9
Ska Lee <ska2519@gmail.com>
2022-08-20 18:17:51 +0300

    setup AuthRepository abstract class

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,32 +1,38 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 import '../../../utils/delay.dart';
 import '../../../utils/in_memory_store.dart';
 import '../domain/app_user.dart';
+import 'auth_repository.dart';
 
-class FakeAuthRepository {
+class FakeAuthRepository implements AuthRepository {
   FakeAuthRepository({this.addDelay = true});
   final bool addDelay;
   final _authState = InMemoryStore<AppUser?>(null);
 
+  @override
   Stream<AppUser?> authStateChanges() => _authState.stream;
+
+  @override
   AppUser? get currentUser => _authState.value;
 
+  @override
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     await delay(addDelay);
     _createNewUser(email);
   }
 
+  @override
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
     await delay(addDelay);
     _createNewUser(email);
   }
 
+  @override
   Future<void> signOut() async {
     _authState.value = null;
   }
 
+  @override
   void dispose() => _authState.close();
 
   void _createNewUser(String email) {
@@ -36,14 +42,3 @@ class FakeAuthRepository {
     );
   }
 }
-
-final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
-  final auth = FakeAuthRepository();
-  ref.onDispose(auth.dispose);
-  return auth;
-});
-
-final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
-  final authRepository = ref.watch(authRepositoryProvider);
-  return authRepository.authStateChanges();
-});
---
+++ lib/src/features/authentication/data/firebase_auth_repository.dart
@@ -0,0 +1,42 @@
+import '../domain/app_user.dart';
+import 'auth_repository.dart';
+
+class FirebaseAuthRepository implements AuthRepository {
+  FirebaseAuthRepository({this.addDelay = true});
+  final bool addDelay;
+
+  // final _authState = InMemoryStore<AppUser?>(null);
+
+  @override
+  Stream<AppUser?> authStateChanges() {
+    // TODO: implement authStateChanges
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> createUserWithEmailAndPassword(String email, String password) {
+    // TODO: implement createUserWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AppUser? get currentUser => throw UnimplementedError();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}

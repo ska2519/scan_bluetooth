4b3cdb2ee618d6599a7ba2e36738e8f0fdc6dae7
Ska Lee <ska2519@gmail.com>
2022-10-14 23:37:51 +0300

    update tablet size appbar

--- lib/src/features/bluetooth/presentation/home_app_bar/home_app_bar.dart
+++ lib/src/features/bluetooth/presentation/home_app_bar/home_app_bar.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import '../../../../constants/resources.dart';
-import '../../../authentication/application/auth_service.dart';
 import '../../../presence_user/presentation/online_user_count.dart';
 import '../../application/scan_bluetooth_service.dart';
 
@@ -16,7 +15,6 @@ class HomeAppBar extends HookConsumerWidget with PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(authStateChangesProvider).value;
     // * This widget is responsive.
     // * On large screen sizes, it shows all the actions in the app bar.
     // * On small screen sizes, it shows only the shopping cart icon and a
@@ -24,48 +22,21 @@ class HomeAppBar extends HookConsumerWidget with PreferredSizeWidget {
     // ! MediaQuery is used on the assumption that the widget takes up the full
     // ! width of the screen. If that's not the case, LayoutBuilder should be
     // ! used instead.
-    final screenWidth = MediaQuery.of(context).size.width;
-    if (screenWidth < Breakpoint.tablet) {
-      return AppBar(
-        title: Stack(
-          children: const [
-            OnlineUserCount(),
-            Center(child: BluetoothCountInfo()),
-          ],
-        ),
-        actions: const [
-          // const ShoppingCartIcon(),
-          // MoreMenuButton(),
+    // final screenWidth = MediaQuery.of(context).size.width;
+    // if (screenWidth < Breakpoint.tablet) {
+    return AppBar(
+      title: Stack(
+        children: const [
+          OnlineUserCount(),
+          Center(child: BluetoothCountInfo()),
         ],
-      );
-    } else {
-      return AppBar(
-        title: const BluetoothCountInfo(),
-        actions: const [
-          // const ShoppingCartIcon(),
-          // if (user != null && !user.isAnonymous!) ...[
-          //   if (!kReleaseMode)
-          //     ActionTextButton(
-          //       key: MoreMenuButton.purchaseKey,
-          //       text: 'Purchase'.hardcoded,
-          //       onPressed: () => context.pushNamed(AppRoute.purchase.name),
-          //     ),
-          //   ActionTextButton(
-          //     key: MoreMenuButton.accountKey,
-          //     text: 'Account'.hardcoded,
-          //     onPressed: () => context.pushNamed(AppRoute.account.name),
-          //   ),
-          // ]
-
-          // else
-          //   ActionTextButton(
-          //     key: MoreMenuButton.signInKey,
-          //     text: 'Sign In'.hardcoded,
-          //     onPressed: () => context.pushNamed(AppRoute.signIn.name),
-          //   )
-        ],
-      );
-    }
+      ),
+      actions: const [
+        // const ShoppingCartIcon(),
+        // MoreMenuButton(),
+      ],
+    );
+    // } else {}
   }
 
   @override

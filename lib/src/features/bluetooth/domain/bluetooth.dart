fa74a8da0554f31d154ac7faa29efc2d8c89f920
Ska Lee <ska2519@gmail.com>
2022-12-02 14:45:27 +0300

    🆕 setting disabledApp feature by RemoteConfig 🆕 setup firebase Remote Config parameter

--- lib/src/features/bluetooth/domain/bluetooth.dart
+++ lib/src/features/bluetooth/domain/bluetooth.dart
@@ -1,12 +1,16 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+import '../../../exceptions/error_logger.dart';
 import '../../firebase/firestore_json_converter.dart';
 import 'label.dart';
 
 part 'bluetooth.freezed.dart';
 part 'bluetooth.g.dart';
+
+//! ScanResult 를 상속받을 수 있는지
 
 @freezed
 class Bluetooth with _$Bluetooth {
@@ -26,6 +30,23 @@ class Bluetooth with _$Bluetooth {
     @Default(false) bool canConnect,
   }) = _Bluetooth;
 
+  @Implements<ScanResult>()
+  factory Bluetooth.scanResult(
+    BluetoothDevice device,
+    AdvertisementData advertisementData,
+    int rssi,
+    DateTime timeStamp,
+  ) {
+    logger.i('Bluetooth.scanResult advertisementData: $advertisementData');
+    return Bluetooth(
+      name: device.name,
+      deviceId: device.id.id,
+      manufacturerDataHead: [],
+      manufacturerData: [],
+      rssi: rssi,
+      scannedAt: timeStamp,
+    );
+  }
   factory Bluetooth.fromJson(Map<String, dynamic> json) =>
       _$BluetoothFromJson(json);
 
